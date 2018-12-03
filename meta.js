c29ddfa9eae3cfa081e22641b22cede868275294
兰晶 <1530562965@qq.com>
2018-12-03 10:58:35 +0800

    更新代码

--- meta.js
+++ meta.js
@@ -1,5 +1,15 @@
 module.exports = {
   prompts: {
+    type:{
+      type: list,
+      message: 'Please select project type',
+      choices: ['模板(scaffolds)','模块(modules)']
+    },
+    mudules:{
+      type: list,
+      message: 'Please project modules',
+      choices: ['aa','bb','cc']
+    },
     name: {
       type: 'string',
       required: true,
@@ -23,7 +33,9 @@ module.exports = {
     }
   },
   filters: {
-
+    'react/**/*': 'templateType==="react"',
+    'vue/**/*': 'templateType==="vue"',
+    'angular/**/*': 'templateType==="angular"',
   },
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev{{/inPlace}}'
 }
